23ebccb990508eef15179aa8a606af8eb5f12fa0
Pat Lackemacher <patrick@lackemacher.com>
2017-01-21 10:44:10 -0500

    Added a couple of tests for ExTwitch to test code coverage recording

--- test/ex_twitch_test.exs
+++ test/ex_twitch_test.exs
@@ -1,8 +1,17 @@
 defmodule ExTwitchTest do
   use ExUnit.Case
+  import ExTwitch
+
   doctest ExTwitch
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "process_url appends argument to base url" do
+    assert process_url("some_path") == "https://api.twitch.tv/kraken/some_path"
+  end
+
+  test "process_request_headers appends accept content type" do
+    headers = process_request_headers([{"Authorization", "OAuth fake_token"}])
+
+    assert Enum.member?(headers, {"Authorization", "OAuth fake_token"})
+    assert Enum.member?(headers, {"Accept", "application/vnd.twitchtv.v5+json"})
   end
 end
