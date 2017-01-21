a588521a6ffbbbd72c638d98e2f8107d2eb49bd9
Pat Lackemacher <patrick@lackemacher.com>
2017-01-21 16:07:50 -0500

    Use test.secret.template.exs if test.secret.exs doesn’t exist

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
-if Mix.env == :test do
+if Mix.env == :test && File.exists?("config/test.secret.exs") do
   import_config "test.secret.exs"
+else
+  import_config "test.secret.template.exs"
 end
