e3b229a11b0cf6aee2c8566b75c91e80fbc34db1
Pat Lackemacher <patrick@lackemacher.com>
2017-01-21 10:00:41 -0500

    Add CI support, code coverage support and flesh out the mix.exs file

--- mix.exs
+++ mix.exs
@@ -1,44 +1,74 @@
 defmodule ExTwitch.Mixfile do
   use Mix.Project
 
+  @description """
+    Simple Elixir wrapper for the Twitch V5 API
+  """
+
   def project do
     [
       app: :ex_twitch,
       version: "0.1.0",
       elixir: "~> 1.3",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      name: "ExTwitch",
+      description: @description,
+      preferred_cli_env: preferred_cli_env(),
+      test_coverage: [tool: ExCoveralls],
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [
-      extra_applications: [
-        :logger,
+      applications: [
         :httpoison,
         :poison
       ]
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.10.0"},
-      {:poison, "~> 3.0"}
+      {:httpoison, "~> 0.10"},
+      {:poison, "~> 3.0"},
+      {:dialyxir, "~> 0.4", only: :dev, runtime: false},
+      {:earmark, "~> 1.0", only: :doc},
+      {:ex_doc, "~> 0.14.5", only: :doc},
+      {:exvcr, "~> 0.6", only: :test},
+      {:meck, "~> 0.8", only: :test},
+      {:excoveralls, "~> 0.6", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: [
+        "Pat Lackemacher",
+      ],
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "https://github.com/plackemacher/ex_twitch"
+      }
+    ]
+  end
+
+  defp preferred_cli_env, do: preferred_cli_env_vcr() ++ preferred_cli_env_coveralls()
+
+  defp preferred_cli_env_vcr do
+    [
+      vcr: :test,
+      "vcr.delete": :test,
+      "vcr.check": :test,
+      "vcr.show": :test,
+    ]
+  end
+
+  defp preferred_cli_env_coveralls do
+    [
+      "coveralls": :test,
+      "coveralls.html": :test,
+      "coveralls.json": :test,
     ]
   end
 end
